0e7842a6837db13ca595ecb1a4945f65d3af71cc
Sam <sam4215@users.noreply.github.com>
2016-02-24 12:05:40 -0600

    Initial Commit

---
+++ src/main/java/me/sam4215/literacygame/Main.java
@@ -0,0 +1,20 @@
+package me.sam4215.literacygame;
+
+import java.*;
+import static java.lang.System.out;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String prompt = "Press SPACEBAR to continue...";
+    String newLine = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+    out.println("Quitter/Camper/Climber: A game");
+    out.println("By Sam Bird, Kami Harker, Brandon Cox, and Emma ..lastname..");
+    out.println(newLine + "YOU: I'll never amount to anything...");
+    out.println(prompt);
+    scanner.nextLine();
+    out.println(newLine + "A VOICE: Finally, someone who shares my views.");
+    out.println(prompt);
+    scanner.nextLine();
+    out.println(newLine + "YOU: Who's there?");
+    out.println("A VOICE: Over here.");
