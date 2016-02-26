79bfe935283eb737eed71e2c17ef33bb5d351a28
Sam <sam4215@users.noreply.github.com>
2016-02-26 12:08:47 -0600

    Finishing story

--- src/main/java/me/sam4215/literacygame/Main.java
+++ src/main/java/me/sam4215/literacygame/Main.java
@@ -1,6 +1,6 @@
 package me.sam4215.literacygame;
 
-import java.*;
+import java.lang.String;
 import java.util.Scanner;
 import static java.lang.System.out;
 
@@ -31,5 +31,22 @@ public class Main {
     out.println("Suddenly, you feel a gust of wind.\n" + prompt);
     scanner.next();
     out.println(newLine + "A VOICE: There's nothing we can do");
+    out.println("YOU: Who's there? And how did you know what I was thinking?\n" + prompt);
+    scanner.next();
+    out.println(newLine + "A VOICE: I'm Superman, and I can read minds.");
+    out.println("YOU: Wow! You're Superman?");
+    out.println("SUPERMAN: Yep. Why do people always NEED me? Would it be too much to ask for some peace and quiet?\n" + prompt);
+    scanner.next();
+    out.println(newLine + "YOU: But you're a superhero! Of course people nee you!\nSUPERMAN: That's what they all say.\nYOU: But...\n" + prompt);
+    scanner.next();
+    out.println(newLine + "YOU (to yourself): Wow. Even superheroes get down sometimes. I'm not going to let anything get me down, because I can be better than Superman!\n" + prompt);
+    scanner.next();
+    out.println(newLine + "YOU: Yeah. I'm going to get out and MAKE stuff!\nA VOICE: That's the spirit!\n" + prompt);
+    scanner.next();
+    out.println(newLine + "YOU: Who are you?\nA VOICE: I'm Kermit, and I thought I was too green. But green can be pretty, too! Now I like myself for the way I am, and you should too.\n" + prompt);
+    scanner.next();
+    out.println(newLine + "YOU: Yeah. I like myself for the way I am! Today's gonna be a good day!\n" + prompt);
+    scanner.next();
+    
   }
 }
